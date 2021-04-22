b752acf4c9f8e03fb22f7d93cee9a033af27bfac
raja_baidyo <35162771+rajabaidyo@users.noreply.github.com>
2021-04-22 19:43:48 +0530

    new update

--- simmons-gui-stage1.py
+++ simmons-gui-stage1.py
@@ -13,18 +13,29 @@ menu = Menu(root)
 item = Menu(menu)
 item.add_command(label='New')
 menu.add_cascade(label='File', menu=item)
-root.config(menu=menu)
+from tkinter import * 
+import string
+import re
+import operator
+
+root = Tk()
+root.title('ATBASH CIPHER')
+root.geometry('500x300')
+
 
 a = Label(root, text = 'Plain-text =>')
-a.grid()
+a.place(x=0,y=35)
 
 
-def simmons(each):
+
+# main driver function
+def atbash(each):
 	# print(each)
 	temp_string=""
 	for val in each : 
 		# print(val,end="")
-		if val in (string.ascii_lowercase or string.ascii_uppercase):
+		#reverses the english alphabet letters
+		if val in string.ascii_lowercase or string.ascii_uppercase:
 			reverse=0
 			if val in string.ascii_lowercase:
 				reverse=122-(ord(val)-97)
@@ -41,7 +52,7 @@ count = 0
 def clicked():
 	global count
 	if (count%2)==0 :
-		result.configure(text = simmons(inp.get()))
+		result.configure(text = atbash(inp.get()))
 		btn.configure(text = 'decrypt!')
 	else :
 		result.configure(text = inp.get())
@@ -55,8 +66,8 @@ def clicked():
 
 
 
-inp = Entry(root, width=10)
-inp.grid(column=1,row=0)
+inp = Entry(root, width=30)
+inp.place(x=100,y=35)
 btn = Button(root,bg = 'blue', text = 'encrypt!',
 	bd = '5',command=clicked)
 btn.place(x=200,y=60)
@@ -68,19 +79,19 @@ btn.place(x=200,y=60)
 
 
 #just the layout part
-lbl = Label(root, text = 'Hello H@ckers!',bg='blue')
-lbl.place(x=200,y=0)
+lbl = Label(root, text = 'Atbash encryption and decryption',bg='blue')
+lbl.place(x=100,y=0)
 
 btin = Button(root,bd = '5', text = 'Exit', command = root.destroy)
 btin.place(x=400,y=0) 
 
-C = Canvas(root, background ="yellow", 
-           height = 250, width = 300) 
+C = Canvas(root, background ="green", 
+           height = 100, width = 300) 
 C.place(x=100,y=100)
 
 
-sim = Label(root,text='simmons experiment',bg='yellow')
-sim.place(x=180,y=100)
+sim = Label(root,text='atbash cipher',bg='yellow')
+sim.place(x=200,y=100)
 
 encrypt = Label(root, text = 'Cipher Text!   ==>> ')
 encrypt.place(x=180,y=120)
